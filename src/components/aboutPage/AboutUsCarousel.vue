27c845c6d998d78dd30e9218edc66c59611e8742
ebelikova <ssdaitro@yandex.ru>
2022-01-12 22:05:54 +0300

    Внесены исправления по замечаниям в компоненты AboutContent и AboutFundBlock

---
+++ src/components/aboutPage/AboutUsCarousel.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="carousel-block">
+    <div class="carousel-block__header-block">
+      <h3 class="carousel-block__title">История фонда</h3>
+      <div class="group-icon">
+        <TripleIcon
+          class="group-icon__arrow-left"
+          @click-icon="onClickPrevSlide"
+          :static="carouselItemActive === 1"
+          icon="ArrowRight"
+          dark
+        />
+        <TripleIcon
+          @click-icon="onClickNextSlide"
+          :static="carouselItemActive === carouselItemAllCount"
+          icon="ArrowRight"
+          dark
+        />
+      </div>
+    </div>
+    <div
+      class="carousel-block__visible-block"
+      :style="{ left: `${carouselItemOffsetLeft}px` }"
+    >
+      <div class="carousel-block__hidden-block">
+        <div
+          v-for="(item, index) in historyList"
+          :key="index"
+          class="carousel-item"
+          :class="{
+            'carousel-item--active':
+              index === carouselItemActive || index === carouselItemActive - 1,
+          }"
+        >
+          <div class="carousel-item__year">{{ item.year }}</div>
+          <div class="carousel-item__line"></div>
+          <p class="carousel-item__text">{{ item.text }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TripleIcon from "../gui/TripleIcon.vue";
+
+export default {
+  data() {
+    return {
+      carouselItemAllCount: 4,
+      carouselItemActive: 1,
+      carouselItemWidth: 500,
+    };
+  },
+  props: ["historyList"],
+  methods: {
+    onClickNextSlide() {
+      if (this.carouselItemActive < this.carouselItemAllCount) {
+        this.carouselItemActive += 1;
+      }
+    },
+
+    onClickPrevSlide() {
+      if (this.carouselItemActive > 1) {
+        this.carouselItemActive -= 1;
+      }
+    },
+  },
+  computed: {
+    carouselItemOffsetLeft() {
+      return -(
+        this.carouselItemActive * this.carouselItemWidth -
+        this.carouselItemWidth
+      );
+    },
+  },
+  components: {
+    TripleIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.group-icon {
+  &__arrow-left {
+    transform: scale(-1, 1);
+  }
+}
+
+.carousel-block {
+  margin-bottom: 0;
+  overflow: hidden;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 48px;
+  }
+
+  &__header-block {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 0 24px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__title {
+    font-size: 18px;
+    line-height: 24px;
+    font-weight: 600;
+    margin-bottom: 24px;
+  }
+
+  &__visible-block {
+    white-space: nowrap;
+    width: 100%;
+    max-width: 960px;
+    padding: 24px;
+    margin: 0 auto;
+    position: relative;
+    transition: all 0.5s ease;
+  }
+
+  &__hidden-block {
+    display: inline-flex;
+    position: relative;
+    left: 0;
+  }
+}
+
+.carousel-item {
+  max-width: 500px;
+  white-space: normal;
+  opacity: 0.5;
+
+  &--active {
+    opacity: 1;
+  }
+
+  &__year {
+    font-size: 34px;
+    line-height: 48px;
+    font-weight: 500;
+  }
+
+  &__line {
+    border-top: 1px solid #e4e4e4;
+    margin: 24px 0;
+    position: relative;
+    width: 100vw;
+
+    &::before {
+      content: "";
+      width: 8px;
+      height: 8px;
+      background-color: black;
+      border-radius: 50%;
+      position: absolute;
+      bottom: -4px;
+    }
+  }
+
+  &__text {
+    padding-right: 72px;
+    font-size: 16px;
+    line-height: 24px;
+    color: #78828c;
+
+    @media screen and (min-width: 768px) {
+      padding-right: 140px;
+    }
+  }
+}
+</style>
