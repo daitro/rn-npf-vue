658614935eda7684b27aa5f9e0f55f602c449a60
ebelikova <ssdaitro@yandex.ru>
2022-03-02 14:41:30 +0300

    Реализована страница отображения одной новости

--- src/api/news.js
+++ src/api/news.js
@@ -1,12 +1,13 @@
 import HTTP from "./http.js";
 
-// На следующей строке дёргаем метод, который прописан
-// в router.news.js на серверной части.
+// На следующей строке объявляем ручку (дёргая метод, который прописан
+// в router.news.js на серверной части)
 // Этот метод возвращает массив
 const getNewsList = () => {
   return HTTP.get("/api/news/list");
 };
 
+//Объявляем ручку для получения статьи по её id, НЕ ДЁРГАЕМ РУЧКУ, а просто объявляем
 ///article/:id
 const getNewsArticleById = (id) => {
   return HTTP.get(`/api/news/article/${id}`);
