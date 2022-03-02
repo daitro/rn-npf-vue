cbfaf72c60e106ad0c05d2d69559aa305285a0b6
ebelikova <ssdaitro@yandex.ru>
2022-03-02 11:39:01 +0300

    Реализовала загрузку новостей с помощью хука created в App.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="app" id="app">
-    <TopMenu></TopMenu>
     <router-view />
     <FooterBlock></FooterBlock>
     <SideBarMenu></SideBarMenu>
@@ -11,13 +10,14 @@
 //@ - это /src
 import FooterBlock from "@/components/app/FooterBlock.vue";
 import SideBarMenu from "@/components/app/SideBarMenu.vue";
-import TopMenu from "@/components/app/TopMenu.vue";
 
 export default {
   components: {
     FooterBlock,
     SideBarMenu,
-    TopMenu,
+  },
+  created() {
+    this.$store.dispatch("news/getNewsList");
   },
 };
 </script>
