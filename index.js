aaecbce7b3bbf9c36d37f562465c999aedeeaae3
ebelikova <ssdaitro@yandex.ru>
2022-04-26 16:39:53 +0300

    вынесла main.js в index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-require('./app/main.js');
+const express = require("express");
+const config = require("config");
+const path = require("path");
+const app = express();
+const port = config.get("PORT") || 3000;
+const newsRoutes = require("./router/news");
+const historyRoutes = require("./router/history");
+const corsMiddleware = require("./middleware/cors");
+
+app.use(corsMiddleware);
+
+app.use("/api/news", newsRoutes);
+app.use("/api/history", historyRoutes);
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "PRODUCTION") {
+	console.log("hi");
+	app.use("/", express.static(path.join(__dirname, "client", "dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
+}
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
